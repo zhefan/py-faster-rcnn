9ddb9d1f7967bbb9749644de50207353f7287b24
Zhefan Ye <zhefanye@gmail.com>
2017-02-19 02:01:14 -0500

    misc update

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -181,8 +181,8 @@ __C.DEDUP_BOXES = 1./16.
 # Pixel mean values (BGR order) as a (1, 1, 3) array
 # We use the same pixel mean for all networks even though it's not exactly what
 # they were trained with
-#__C.PIXEL_MEANS = np.array([[[102.9801, 115.9465, 122.7717]]])
-__C.PIXEL_MEANS = np.array([[[99.5109, 104.4950, 119.9784]]])
+__C.PIXEL_MEANS = np.array([[[102.9801, 115.9465, 122.7717]]])
+#__C.PIXEL_MEANS = np.array([[[99.5109, 104.4950, 119.9784]]])
 
 # For reproducibility
 __C.RNG_SEED = 3
